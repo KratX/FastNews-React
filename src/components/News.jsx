a27f5a910794712d6abcccb686eeb1a91fc07c29
[Kartik Rawat] <kartikrawat9@gmail.com>
2023-08-01 01:26:56 +0530

    Added APIs and corrected rendering

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,19 @@
 import { NewsItem } from "./NewsItem";
+import { useQuery } from "@tanstack/react-query";
 
 export function News() {
+  const HeadlinesQuery = useQuery({
+    queryKey: ["topHeadlines"], // unique identifier for this query
+    queryFn:  async () => {
+      const response = await fetch(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=9080c2e52b9440e4950be63abc91c41a"
+      );
+      const data = await response.json();
+      console.log(data);
+      return data.articles;
+    },
+  });
+
   return (
     <div className="bg-gray-500">
       <div className="flex flex-col items-center gap-6 text-center px-4">
@@ -11,36 +24,24 @@ export function News() {
             lg:text-7xl"
         >
           <span className="mt-4 inline-block text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-amber-500">
-            Top Headlines based on  
+            Top Headlines based on
           </span>
         </h1>
       </div>
-      <div className="flex flex-wrap">
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-        <div className="my-12 mx-auto">
-          <NewsItem title="my title" description="my desc" />
-        </div>
-      </div>
+      <div className="flex flex-wrap" >
+      {HeadlinesQuery.data?.map((element) => {
+        return (
+            <div className="my-12 mx-auto" key={element.url}>
+              <NewsItem
+                title={element.title}
+                description={element.description}
+                urlToImage={element.urlToImage}
+                newsUrl={element.url}
+              />
+            </div>
+        );
+      })}
+          </div>
     </div>
   );
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,4 +1,4 @@
-import {Card,CardHeader,CardBody,CardFooter,Typography,Button} from "@material-tailwind/react";
+import {Card,CardHeader,CardBody,CardFooter,Typography} from "@material-tailwind/react";
 import PropTypes from 'prop-types'
 
    
@@ -8,11 +8,11 @@ import PropTypes from 'prop-types'
       <Card className="mt-8 mx-4 lg:w-96 w-[22rem]">
         <CardHeader color="blue-gray" className="relative h-56">
           <img
-            src=""
+            src={props.urlToImage}
             alt="card-image"
           />
         </CardHeader>
-        <CardBody>
+        <CardBody className="h-64">
           <Typography variant="h5" color="blue-gray" className="mb-2">
             {props.title}
           </Typography>
@@ -21,7 +21,23 @@ import PropTypes from 'prop-types'
           </Typography>
         </CardBody>
         <CardFooter className="pt-0">
-          <Button>Read More</Button>
+        <a size="sm" href={props.newsUrl} rel="noreferrer" target="_blank"  className="flex items-center gap-2 font-bold text-light-blue-800">
+            Learn More
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </a>
         </CardFooter>
       </Card>
     );
@@ -30,4 +46,6 @@ import PropTypes from 'prop-types'
   NewsItem.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
+    urlToImage: PropTypes.string,
+    newsUrl: PropTypes.string,
   }
