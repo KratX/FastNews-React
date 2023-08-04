8845c4b192a5b972751a3da4b290e883da0109a8
[Kartik Rawat] <kartikrawat9@gmail.com>
2023-08-04 23:41:59 +0530

    Added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Navigationbar } from "./components/Navbar";
+import { News } from "./components/News";
+import { Route, Routes } from "react-router-dom";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigationbar />
+      <Routes>
+      <Route exact path="/" element={<News/>}></Route>
+      </Routes>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Navigationbar } from "./components/Navbar.jsx";
-import { News } from "./components/News.jsx";
-
-const queryClient = new QueryClient();
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Navigationbar />
-      <News category="sports" />
-    </QueryClientProvider>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
